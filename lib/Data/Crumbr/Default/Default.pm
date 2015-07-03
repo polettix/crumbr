df6d1fd080f6b862e4d42163ee25876958c0a817
Flavio Poletti <flavio@polettix.it>
2015-07-03 17:50:34 +0200

    empty profile for default values

---
+++ lib/Data/Crumbr/Default/Default.pm
@@ -0,0 +1,9 @@
+package Data::Crumbr::Default::Default;
+
+# ABSTRACT: "Default" profile for Data::Crumbr::Default
+
+# Default is default... nothing is set here!
+sub profile { return {}; }
+
+1;
+__END__
