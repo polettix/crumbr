bf9a014210b4828a7c207e8662710b8daa789bb3
Flavio Poletti <flavio@polettix.it>
2015-07-05 01:00:48 +0200

    added some documentation

--- lib/Data/Crumbr.pm
+++ lib/Data/Crumbr.pm
@@ -125,8 +125,304 @@ sub encode {
 } ## end sub encode
 
 1;
+
+=pod
+
+=head1 SYNOPSIS
+
+   use Data::Crumber; # imports `crumbr`
+
+   # some data to work with
+   my $data = { what => 'ever', hey => 'you' };
+
+   # crumbr provides an anonyous sub back. This has defaults
+   my $csub = crumbr();
+
+   # use it to encode the data
+   my $encoded = $crumbr->($data);
+   # {"here"}{"hey"}:"you"
+   # {"here"}{"what"}:"ever"
+
+   # URI profile simplifies things but loses something
+   $encoded = crumbr(profile => 'URI')->($data);
+   # here/hey "you"
+   # here/what "ever"
+
+   # JSON profile produces valid JSON "slices"
+   $encoded = crumbr(profile => 'JSON')->($data);
+   # {"here":{"hey":"you"}}
+   # {"here":{"what":"ever"}}
+
+   # Object Oriented Interface
+   my $crobj = Data::Crumbr->new();
+   $encoded = $crobj->encode($data); # same as default
+
+
+=head1 DESCRIPTION
+
+Data::Crumbr lets you render data structures in a way that can then be
+easily searched and parsed in "slices". The basic idea is that data
+shaped in this way will then be easily filtered in the shell for
+extracting interesting parts.
+
+The input data structure is traversed is if it is a tree (so no
+circular structures please!), and a I<record> is generated for each leaf
+in the tree. Depending on the backend and the configurations, the full
+path from the root to the parent of the leaf is represented as a
+sequence of keys (which can be hash keys or array indexes) followed by
+the value. This should make your life easier e.g. in the shell, so that
+you can specify the full path to the data structure part you're
+interested into with common Unix tools like C<grep> and/or C<sed>.
+
+=head2 Example
+
+Suppose you have the following data structure in Perl:
+
+   my $data = {
+      one => '1',
+      two => 2,
+      three => 3.1,
+      four => '4.0',
+      true => \1,
+      false => \0,
+      array => [
+         qw< what ever >,
+         { inner => 'part', empty => [] }
+       ],
+      hash => {
+         'with ♜' => {},
+         ar => [ 1..3 ],
+         something => "funny \x{263A} ☻",
+      },
+   };
+
+If you encode this e.g. in JSON, it will be easy to parse with
+the right program, but not from the shell, even if you pretty
+print it:
+
+   {
+      "hash" : {
+         "something" : "funny ☺ ☻",
+         "with ♜" : {},
+         "ar" : [
+            1,
+            2,
+            3
+         ]
+      },
+      "one" : "1",
+      "array" : [
+         "what",
+         "ever",
+         {
+            "inner" : "part",
+            "empty" : []
+         }
+      ],
+      "four" : "4.0",
+      "true" : true,
+      "two" : 2,
+      "three" : 3.1,
+      "false" : false
+   }
+
+How do you get the second item in the array C<ar>i inside the
+hash C<hash>? Would you do better with YAML instead?
+
+   ---
+   array:
+     - what
+     - ever
+     - empty: []
+       inner: part
+   false: !!perl/ref
+     =: 0
+   four: 4.0
+   hash:
+     ar:
+       - 1
+       - 2
+       - 3
+     something: funny ☺ ☻
+     with ♜: {}
+   one: 1
+   three: 3.1
+   true: !!perl/ref
+     =: 1
+   two: 2
+
+Not really. Data::Crumbr lets you represent the data in a
+more verbose but easily consumable way for the shell. Hence,
+this:
+
+   use Data::Crumbr;
+   print crumbr()->($data), "\n";
+
+will give you this:
+
+   {"array"}[0]:"what"
+   {"array"}[1]:"ever"
+   {"array"}[2]{"empty"}:[]
+   {"array"}[2]{"inner"}:"part"
+   {"false"}:false
+   {"four"}:"4.0"
+   {"hash"}{"ar"}[0]:1
+   {"hash"}{"ar"}[1]:2
+   {"hash"}{"ar"}[2]:3
+   {"hash"}{"something"}:"funny \u263A \u263B"
+   {"hash"}{"with \u265C"}:{}
+   {"one"}:"1"
+   {"three"}:3.1
+   {"true"}:true
+   {"two"}:2
+
+Now it should pretty easy for a shell program to get at the
+data, e.g. with this C<sed> substitution:
+
+   sed -ne 's/^{"hash"}{"ar"}\[2\]://p'
+
+=head2 Profiles
+
+If you don't like the default encoding, you can get a different
+one by using a I<profile>. This is a set of configurations for
+C<Data::Crumbr::Default>, which is a pretty generic class for
+representing a wide class of possible record-oriented encodings.
+
+A C<Data::Crumbr::Default> encoder is defined in terms of the following
+parameters:
+
+=over
+
+=item C<array_open>
+
+sequence to put when an array is opened
+
+=item C<array_close>
+
+sequence to put when an array is closed
+
+=item C<array_key_prefix>
+
+sequence to put before an array's index
+
+=item C<array_key_suffix>
+
+sequence to put after an array's index
+
+=item C<array_key_encoder>
+
+a reference to a function that encodes an array's index
+
+=item C<hash_open>
+
+sequence to put when a hash is opened
+
+=item C<hash_close>
+
+sequence to put when a hash is closed
+
+=item C<hash_key_prefix>
+
+sequence to put before a hash's key
+
+=item C<hash_key_suffix>
+
+sequence to put after a hash's key
+
+=item C<hash_key_encoder>
+
+a reference to a function that encodes a hash's key
+
+=item C<value_encoder>
+
+a reference to a function that encodes a leaf value
+
+=item C<keys_separator>
+
+sequence to separate the keys breadcrumb
+
+=item C<value_separator>
+
+sequence to separate the keys from the value
+
+=back
+
+By default, Data::Crumbr ships with the following profiles:
+
+=over
+
+=item B<< Default >>
+
+i.e. the profile you get by default, and what you saw in action in the
+example above. It has the following settings:
+
+=over
+
+=item *
+
+no openers and closers:
+
+   array_open  => ''
+   array_close => ''
+   hash_open   => ''
+   hash_close  => ''
+
+=item *
+
+array keys are printed verbatim, surrounded by square brackets:
+
+   array_key_prefix  => '['
+   array_key_suffix  => ']'
+   array_key_encoder => Data::Crumbr::Util::id_encoder
+
+=item *
+
+hash keys encoded as JSON strings, surrounded by curly brackets:
+
+   hash_key_prefix   => '['
+   hash_key_suffix   => ']'
+   hash_key_encoder  => Data::Crumbr::Util::json_leaf_encoder
+
+=item *
+
+no separator between keys (because they already stand out very clearly,
+but a colon to separate the sequence of keys from the value:
+
+   keys_separator  => ''
+   value_separator => ':'
+
+=item *
+
+leaf values encoded as JSON scalars:
+
+   value_encoder => Data::Crumbr::Util::json_leaf_encoder
+
+=back
+
+This is quite verbose, but lets you specify very precisely what you are
+looking for because the hash keys stand out clearly with respect to
+array identifiers, i.e. there's no chance that you will mistake an array
+index for a hash key (because they are embedded in different bracket
+types).
+
+
+=item B<< >>
+
+=back
+
+
+
+If your data structure is simple and you have control over it,
+you can also ask Data::Crumbr to be less verbose by using
+the I<profile>
+
+=head1 INTERFACE
+
+There are two ways to use Data::Crumber: a function C<crumbr>,
+that is exported by default, and the object-oriented interface.
+
+
+
+=cut
+
 __END__
-
-SYNOPSYS
-
-   whatever
